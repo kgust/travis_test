d679bbf829e8ae8bfc15bbd30e5ec62d512d36a3
Kevin Gustavson <kgustavson@celltrak.com>
2013-02-06 08:36:21 -0600

    Add a simple unit test to test Travis.

---
+++ Tests/SameTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This tests the PermissionSet class.
+ */
+class PermissionSetTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @group unit
+     * @group core
+     */
+    public function testEquals()
+    {
+        $this->assertEquals(1, true);
+    }
+
+
+}
